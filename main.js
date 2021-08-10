b43289016ca8101da3b7697ac0f3d122df2e742d
KIARA SMALL <SMALLKAY1726@GMAIL.COM>
2021-08-10 13:50:22 -0500

    commit on main

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 // sanity test
 //console.log ("UNICORNS")
+
+//Create a constructor function for:
+//Name of the product
+//File path of image
+//Times the image has been shown
+
+class busmallPictures{
+    // constructor function for the ENTIRE project.
+
+    clicks = 0; //amount of clicks
+    timeShown = 0; // amount of times of image
+
+    // Initial constructor (Must be named CONSTUCTOR)
+    constructor (name , imageSrc)
+    {
+        this.name = name; // image name
+        this.imageSrc = imageSrc; // the source to the image
+    };
+}
+
+// pictures that i want displayed

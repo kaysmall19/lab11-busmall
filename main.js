be3d874413708b657c57a658f321075cfdfca72e
KIARA SMALL <SMALLKAY1726@GMAIL.COM>
2021-08-10 16:01:24 -0500

    commit on main

--- main.js
+++ main.js
@@ -6,7 +6,8 @@
 //File path of image
 //Times the image has been shown
 
-class busmallPictures{
+class busmallPictures
+{
     // constructor function for the ENTIRE project.
 
     clicks = 0; //amount of clicks
@@ -21,3 +22,10 @@ class busmallPictures{
 }
 
 // pictures that i want displayed
+let AllbusmallPictures = 
+{
+    new busmallPictures("banana", "busmall-project/busmall-pics/banana.jpg"),
+    new busmallPictures("breakfast", "busmall-project/busmall-pics/breakfast.jpg"),
+    new busmallPictures("unicorn", "busmall-project/busmall-pics/unicorn.jpg"),
+    new busmallPictures("wine-glass", "busmall-project/busmall-pics/wine-glass.jpg")
+}
